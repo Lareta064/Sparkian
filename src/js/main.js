89605178e71e942d42a590b32a75d28b0d8268a6
Lareta064 <lareta064@gmail.com>
2023-10-07 09:00:27 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -38,4 +38,52 @@ document.addEventListener("DOMContentLoaded", function () {
 			}
 		});
 	}
+ 
+    /* game select */
+	const mySelectBlocks = Array.from(document.getElementsByClassName('mySelect'));
+	mySelectBlocks.forEach((item, i) => {
+		const mySelect = item.querySelector('.mySelect-input');
+		// const mySelectInput = item.querySelector('.selectValue');
+		let mySelectOptions = item.querySelectorAll('.mySelect-options');
+		const mySelectIcon = item.querySelector('.mySelect-icon');
+		const mySelecDrop = item.querySelector('.mySelect-drop');
+
+		mySelect.addEventListener('click', () => {
+
+			if (mySelecDrop.classList.contains('active')) {
+				mySelecDrop.classList.remove('active');
+				mySelectIcon.classList.remove('active');
+				mySelect.classList.remove('open');
+
+
+			} else {
+				mySelecDrop.classList.add('active');
+				mySelectIcon.classList.add('active');
+				mySelect.classList.add('open');
+			}
+
+		});
+		for (let item of mySelectOptions) {
+			item.addEventListener('click', () => {
+				itemText = item.querySelector('.mySelect-options__value').innerText;
+				mySelecDrop.classList.remove('active');
+				mySelectIcon.classList.remove('active');
+				mySelect.innerText = itemText;
+				for (let i = 0; i < mySelectOptions.length; i++){
+					mySelectOptions[i].classList.remove('active-item');
+				}
+				item.classList.add('active-item');
+				
+
+			});
+		}
+
+	});
+
+	var mixer = mixitup('#mixGameCards', {
+		
+		animation: {
+			duration: 300
+		}
+	});
 })
